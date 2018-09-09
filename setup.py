f1242d5897ba4f156a87fe2e417cdc88138644f0
Nagarjuna Kumar <nagarjuna.kumar@duedil.com>
2018-09-09 01:15:34 +0100

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='corella',
-      version='0.1',
+      version='0.0.2',
       description='correlation matrix plots on the terminal',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/nk412/corella',
       author='Nagarjuna Kumarappan',
       author_email='nagarjuna.412@gmail.com',
       license='MIT',
       packages=['corella'],
       install_requires=[
-          'pandas', 'colored'
+          'pandas>=0.23.4', 'colored>=1.3.93'
       ],
       entry_points={
         'console_scripts': ['corella=corella.cli:main']
